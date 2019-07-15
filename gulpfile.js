5bdb18c9e49cde44da5bb73c6049c8e47800db6f
mallowigi <heliosaian@gmail.com>
2019-07-15 23:40:23 +0300

    take styles

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+
+const paths = {
+    styles: {
+        src: 'scss/*.scss',
+        dest: 'dist'
+    }
+};
+
+gulp.task('styles', () => {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename({
+            basename: 'slack.min'
+        })).pipe(gulp.dest(paths.styles.dest));
+});
+
+gulp.task('debugStyles', () => {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(rename({
+            basename: 'slack'
+        })).pipe(gulp.dest(paths.styles.dest));
+});
+
+gulp.task('watchStyles', () => {
+    gulp.watch(paths.styles.src, gulp.parallel('debugStyles'));
+});
