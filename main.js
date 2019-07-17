e9c60d41fbbf6480c9c2c00a0b76cebe2aced92b
Elior <eliorboukhobza@Eliors-MacBook-Pro.local>
2019-07-17 18:19:14 +0300

    push

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const os = require('os');
 const osType = os.type();
 const homeDir = os.homedir();
 
+// TODO add multitheme support
 const NAME = 'One Dark';
 const REPO = 'https://raw.githubusercontent.com/mallowigi/slack-one-dark-theme/master/dist/slack.min.css';
 
@@ -69,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });`;
 
 const WIN_DIR = `${homeDir}\\AppData\\Local\\slack\\`;
-const MAC_DIR = '/Applications/Slack\ Copy.app/Contents/Resources';
+const MAC_DIR = '/Applications/Slack.app/Contents/Resources';
 const LINUX_DIR = `/usr/lib/slack`;
 
 function getDirectory() {
@@ -90,12 +91,33 @@ const SLACK_RESOURCES_DIR = getDirectory();
 const SLACK_FILE_PATH = `${SLACK_RESOURCES_DIR}/app.asar.unpacked/dist/ssb-interop.bundle.js`;
 const SLACK_ARCHIVE = `${SLACK_RESOURCES_DIR}/app.asar`;
 
-console.log(`Adding ${NAME} Theme Code to Slack... `);
+const apply = () => {
+  console.log(`Adding ${NAME} Theme Code to Slack... `);
 
-// Make a backup
-fs.copyFileSync(SLACK_ARCHIVE, `${SLACK_RESOURCES_DIR}/app.asar.backup`);
-// Extract archive
-asar.extractAll(`${SLACK_ARCHIVE}`, `${SLACK_RESOURCES_DIR}/app.asar.unpacked`);
-// Add js code and recreate the package
-fs.appendFileSync(SLACK_FILE_PATH, JS);
-asar.createPackage(`${SLACK_RESOURCES_DIR}/app.asar.unpacked`, `${SLACK_RESOURCES_DIR}/app.asar`);
+  // Make a backup
+  fs.copyFileSync(SLACK_ARCHIVE, `${SLACK_RESOURCES_DIR}/app.asar.backup`);
+  // Extract archive
+  asar.extractAll(`${SLACK_ARCHIVE}`, `${SLACK_RESOURCES_DIR}/app.asar.unpacked`);
+  // Add js code and recreate the package
+  fs.appendFileSync(SLACK_FILE_PATH, JS);
+  asar.createPackage(`${SLACK_RESOURCES_DIR}/app.asar.unpacked`, `${SLACK_RESOURCES_DIR}/app.asar`);
+};
+
+const restore = () => {
+  console.log(`Restoring Backup... `);
+
+  fs.copyFile(`${SLACK_RESOURCES_DIR}/app.asar.backup`, `${SLACK_RESOURCES_DIR}/app.asar`);
+};
+
+// Start
+const myArgs = process.argv.slice(2);
+switch (myArgs[0]) {
+  case 'apply':
+    apply();
+    break;
+  case 'restore':
+    restore();
+    break;
+  default:
+    console.error('Unknown command:', myArgs[0])
+}
--- restore.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const SLACK_RESOURCES_DIR="/Applications/Slack\ Copy.app/Contents/Resources"
-const SLACK_FILE_PATH=`${SLACK_RESOURCES_DIR}/app.asar.unpacked/dist/ssb-interop.bundle.js`;
-const SLACK_ARCHIVE = `${SLACK_RESOURCES_DIR}/app.asar`;
-
-console.log('Restoring backup');
-
-fs.copyFile(`${SLACK_RESOURCES_DIR}/app.asar.backup`, `${SLACK_RESOURCES_DIR}/app.asar`);
