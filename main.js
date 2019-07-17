7bc2366de90342ddef0da1e765598a4de90e64a2
Elior <eliorboukhobza@Eliors-MacBook-Pro.local>
2019-07-17 19:12:02 +0300

    new cli

--- main.js
+++ main.js
@@ -1,123 +1,22 @@
 #! /usr/bin/env node
-const asar = require('asar');
-const fs = require('fs');
-const os = require('os');
-const osType = os.type();
-const homeDir = os.homedir();
-
-// TODO add multitheme support
-const NAME = 'One Dark';
-const REPO = 'https://raw.githubusercontent.com/mallowigi/slack-one-dark-theme/master/dist/slack.min.css';
-
-const CSS = `
-:root {
-    /* Modify these to change your theme colors: */
-   --primary: #E5C17C;
-   --accent: #568AF2;
-   --text: #ABB2BF;
-   --background: #282C34;
-   --background-elevated: #3B4048;
-
-   /* These should be less important: */
-   --background-hover: #525964;
-   --background-light: #AAA;
-   --background-bright: #FFF;
-
-   --border-dim: #666;
-   --border-bright: var(--primary);
-
-   --text-bright: #FFF;
-   --text-dim: #555c69;
-   --text-special: var(--primary);
-   --text-accent: var(--text-bright);
-
-   --scrollbar-background: #000;
-   --scrollbar-border: var(--primary);
-
-   --yellow: #fc0;
-   --green: #98C379;
-   --cyan: #56B6C2;
-   --blue: #61AFEF;
-   --purple: #C678DD;
-   --red: #E06C75;
-   --red2: #BE5046;
-   --orange: #D19A66;
-   --orange2: #E5707B;
-   --gray: #3E4451;
-   --silver: #9da5b4;
-   --black: #21252b;
-}
-`;
-
-// The JS we want to inject
-const JS = `
-document.addEventListener('DOMContentLoaded', function() {
-  // Fetch our CSS in parallel ahead of time
-  const cssPath = '${REPO}';
-  let cssPromise = fetch(cssPath).then((response) => response.text());
-
-  let customCustomCSS = \`
-   ${CSS}
-   \`;
-
-  // Insert a style tag into the wrapper view
-  cssPromise.then((css) => {
-    let s = document.createElement('style');
-    s.type = 'text/css';
-    s.innerHTML = css + customCustomCSS;
-    document.head.appendChild(s);
-  });
-});`;
-
-const WIN_DIR = `${homeDir}\\AppData\\Local\\slack\\`;
-const MAC_DIR = '/Applications/Slack.app/Contents/Resources';
-const LINUX_DIR = `/usr/lib/slack`;
-
-function getDirectory() {
-  switch (osType) {
-    case 'Linux':
-      return LINUX_DIR;
-    case 'Windows_NT':
-      return WIN_DIR;
-    case 'Darwin':
-      return MAC_DIR;
-    default:
-      return MAC_DIR;
-  }
-}
-
-const SLACK_RESOURCES_DIR = getDirectory();
-
-const SLACK_FILE_PATH = `${SLACK_RESOURCES_DIR}/app.asar.unpacked/dist/ssb-interop.bundle.js`;
-const SLACK_ARCHIVE = `${SLACK_RESOURCES_DIR}/app.asar`;
-
-const apply = () => {
-  console.log(`Adding ${NAME} Theme Code to Slack... `);
-
-  // Make a backup
-  fs.copyFileSync(SLACK_ARCHIVE, `${SLACK_RESOURCES_DIR}/app.asar.backup`);
-  // Extract archive
-  asar.extractAll(`${SLACK_ARCHIVE}`, `${SLACK_RESOURCES_DIR}/app.asar.unpacked`);
-  // Add js code and recreate the package
-  fs.appendFileSync(SLACK_FILE_PATH, JS);
-  asar.createPackage(`${SLACK_RESOURCES_DIR}/app.asar.unpacked`, `${SLACK_RESOURCES_DIR}/app.asar`);
-};
-
-const restore = () => {
-  console.log(`Restoring Backup... `);
-
-  fs.copyFile(`${SLACK_RESOURCES_DIR}/app.asar.backup`, `${SLACK_RESOURCES_DIR}/app.asar`);
-};
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const cli = require('./lib/cli');
+const {execute} = require('./lib/command');
 
 // Start
-const myArgs = process.argv.slice(2);
-switch (myArgs[0]) {
-  case 'apply':
-    apply();
-    break;
-  case 'restore':
-    restore();
-    break;
-  default:
-    console.error('Unknown command:', myArgs[0])
+async function main() {
+  clear();
+  console.log(chalk.yellow(figlet.textSync('Slack Theme Applier')));
+
+  console.log(chalk.cyan('Welcome to the Slack Theme Applier CLI!'));
+  console.log('');
+  console.log(chalk.red('Please note that this is still experimental and can ruin your Slack application! In the case of a problem, please reinstall Slack.'));
+  console.log('');
+  const {ask: answer} = await cli.ask();
+
+  execute(answer);
 }
+
+main();
