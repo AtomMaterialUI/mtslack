a1dfe9f0bb2452089e8650cbcfcf92dcd12f55b2
helio <heliosaian@gmail.com>
2020-03-21 18:12:47 +0200

    checking autoupdate

--- main.js
+++ main.js
@@ -6,11 +6,7 @@ const isRoot = require('is-root');
 const cli = require('./lib/cli');
 const {execute} = require('./lib/command');
 
-// Start
-async function main() {
-  clear();
-  checkForUpdates();
-
+async function afterUpdate() {
   console.log(chalk.yellow(figlet.textSync('Slack Theme Applier')));
 
   console.log(chalk.cyan('Welcome to the Slack Theme Applier CLI!'));
@@ -18,7 +14,7 @@ async function main() {
   console.log(chalk.red('Please note that this is still experimental and can ruin your Slack application! In the case of a problem, please reinstall Slack.'));
   console.log('');
 
-  if (!isRoot()){
+  if (!isRoot()) {
     console.log(chalk.red('You must be root to execute this command. Run with "sudo mtslack"'));
     process.exit(1);
   }
@@ -26,6 +22,12 @@ async function main() {
   const {ask: answer} = await cli.ask();
 
   execute(answer);
+}
+
+// Start
+async function main() {
+  clear();
+  checkForUpdates();
 }
 
 function checkForUpdates(){
@@ -42,6 +44,7 @@ function checkForUpdates(){
   });
   update.on('finish', () => {
     console.log(chalk.green('finished updating!'));
+    afterUpdate();
   });
 }
 
