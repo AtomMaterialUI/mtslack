cc1d56cc266916410c1b3ccd64fba21cb29b832d
Elior <heliosaian@gmail.com>
2021-11-11 22:50:17 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const fileInclude = require('gulp-browser-js-include');
@@ -8,40 +8,51 @@ const injectVersion = require('gulp-inject-version');
 const paths = {
   styles: {
     src: 'styles/**/*.scss',
-    dest: 'dist'
+    dest: 'dist',
   },
   scripts: {
     src: 'includes/**/*.js',
     main: 'includes/**/plugins.js',
-    dest: 'dist'
-  }
+    dest: 'dist',
+  },
 };
 
 gulp.task('styles', () => {
-  return gulp.src(paths.styles.src)
+  return gulp
+    .src(paths.styles.src)
     .pipe(sass())
     .pipe(injectVersion())
     .pipe(cleanCSS())
-    .pipe(rename({
-      basename: 'slack.min'
-    })).pipe(gulp.dest(paths.styles.dest));
+    .pipe(
+      rename({
+        basename: 'slack.min',
+      })
+    )
+    .pipe(gulp.dest(paths.styles.dest));
 });
 
 gulp.task('debugStyles', () => {
-  return gulp.src(paths.styles.src)
+  return gulp
+    .src(paths.styles.src)
     .pipe(sass())
     .pipe(injectVersion())
-    .pipe(rename({
-      basename: 'slack'
-    })).pipe(gulp.dest(paths.styles.dest));
+    .pipe(
+      rename({
+        basename: 'slack',
+      })
+    )
+    .pipe(gulp.dest(paths.styles.dest));
 });
 
 gulp.task('scripts', () => {
-  return gulp.src(paths.scripts.main)
-    .pipe(injectVersion())
-    .pipe(fileInclude())
-    // .pipe(butternut())
-    .pipe(gulp.dest(paths.scripts.dest));
+  return (
+    gulp
+      .src(paths.scripts.main)
+      .pipe(injectVersion())
+      .pipe(fileInclude())
+      // .pipe(butternut())
+      .pipe(gulp.dest(paths.scripts.dest))
+  );
 });
 
 gulp.task('watchStyles', () => {
