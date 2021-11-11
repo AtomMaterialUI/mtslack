34cb47018ab5d5c14fcf7350843e94ad303dc07b
Elior <heliosaian@gmail.com>
2021-11-11 10:19:22 +0200

    fix deps

--- main.js
+++ main.js
@@ -3,31 +3,37 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const isRoot = require('is-elevated');
-const sample  = require('@feizheng/next-sample');
+const sample = require('@feizheng/next-sample');
 const cli = require('./lib/cli');
-const {execute} = require('./lib/command');
+const { execute } = require('./lib/command');
 const pkg = require('./package.json');
 
 async function run() {
-  console.log(chalk.yellow(figlet.textSync('mtslack', {
-    font: sample(['Whimsy']),
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-    width: 80,
-    whitespaceBreak: true
-  })));
+  console.log(
+    chalk.yellow(
+      figlet.textSync('mtslack', {
+        font: sample(['Whimsy']),
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true,
+      })
+    )
+  );
   console.log(chalk.italic(`version ${pkg.version} by @mallowigi`));
 
   console.log(chalk.cyan('Welcome to the mtslack CLI!'));
   console.log('');
 
   if (!isRoot()) {
-    console.log(chalk.red('You must be root to execute this command. Run with "sudo mtslack" (or run as a Windows Administrator)'));
+    console.log(
+      chalk.red('You must be root to execute this command. Run with "sudo mtslack" (or run as a Windows Administrator)')
+    );
     process.exit(1);
   }
 
   // noinspection JSDeclarationsAtScopeStart
-  const {ask: answer} = await cli.ask();
+  const { ask: answer } = await cli.ask();
 
   execute(answer);
 }
@@ -55,8 +61,7 @@ async function checkForUpdates() {
     if (shouldUpdate) {
       console.log('Update finished. Please rerun the command.');
       process.exit(0);
-    }
-    else {
+    } else {
       await run();
     }
   });
